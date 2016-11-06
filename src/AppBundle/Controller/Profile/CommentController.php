41f0dd526809d00559cf34537c0be1026e6c69b3
Jeanphinov <hello@jiphi.be>
2016-11-06 04:49:52 +0100

    Affichage des commentaires -> enlevé du répertoire Admin vers répertoire profile.   Pas certain du repertoire destination mais mieux que Admin.

---
+++ src/AppBundle/Controller/Profile/CommentController.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace AppBundle\Controller\Profile;
+
+use AppBundle\AppBundle;
+use AppBundle\Entity\Commentaire;
+use AppBundle\Entity\Prestataire;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+
+class CommentController extends Controller
+{
+
+    /**
+     * @Route("/prestataire/comment/{id}", name="prestataire_commentaire_view")
+     */
+    public function viewCommentPrestataireAction($id)
+    {
+        $manager = $this->getDoctrine()->getManager();
+        $repo = $manager->getRepository('AppBundle\Entity\Commentaire');
+        $commentaires = $repo->findByPrestataire(["id" => $id]);
+
+
+        return $this->render('_partials/bloc/_bloc-commentaires.html.twig', ["commentaires" => $commentaires]);
+    }
+
+    /**
+     * @Route("/admin/comment/new", name="admin_comment_create")
+     */
+    public function createAction(Request $request)
+    {
+        // ici viendra le code qui renvoie vers le formulaire creation d'un comment (Admin)
+        return $this->render('admin/comment/comment-create.html.twig');
+    }
+
+    /**
+     * @Route("/admin/comment/update", name="admin_comment_update")
+     */
+    public function updateAction(Request $request)
+    {
+        // ici viendra le code qui renvoie vers le formulaire update d'un comment (Admin)
+        return $this->render('admin/comment/comment-update.html.twig');
+    }
+
+    /**
+     * @Route("/admin/comment/delete", name="admin_comment_delete")
+     */
+    public function deleteAction(Request $request)
+    {
+        // ici viendra le code qui renvoie vers le formulaire suppression comment (Admin)
+        return $this->render('admin/comment/comment-delete.html.twig');
+    }
+
+
+}
